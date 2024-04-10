4078489da3058d02db7f20d270caf089126f0c80
Ashish Chaudhary <chauashish21@gmail.com>
2024-04-10 18:49:30 +0530

    Connect To mysql

--- LearnHub/learnhub_api/learnhub_api/settings.py
+++ LearnHub/learnhub_api/learnhub_api/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'learnhub_api.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'learnhub',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
