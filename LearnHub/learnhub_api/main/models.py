27e6a5f1a336b4b00c1332e487a497ac69902611
Ashish Chaudhary <chauashish21@gmail.com>
2024-04-13 14:34:02 +0530

    Started Rest Api

--- LearnHub/learnhub_api/main/models.py
+++ LearnHub/learnhub_api/main/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+#Teacher Model
+class Teacher(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+    qualification = models.CharField(max_length=200)
+    mobile = models.BigIntegerField()
+    address = models.TextField()
+    
+    class Meta:
+        verbose_name_plural = "Teachers"
+    
+#Student Model
+class Student(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+    qualification = models.CharField(max_length=200)
+    mobile = models.BigIntegerField()
+    address = models.TextField()
+    interested_categories = models.TextField()
+    
+    class Meta:
+        verbose_name_plural = "Students"
+    
+#Course Categories
+class CourseCategory(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    
+    class Meta:
+        verbose_name_plural = "Categories"
+    
+#Course Model
+class Course(models.Model):
+    category = models.ForeignKey(CourseCategory, on_delete = models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete = models.CASCADE)
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    
+    class Meta:
+        verbose_name_plural = "Courses"
